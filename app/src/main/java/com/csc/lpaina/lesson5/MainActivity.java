ec27e3a200ee06ae1b3847765310e8806339a113
lpaina <lubashka.994@mail.ru>
2016-03-28 01:57:13 +0300

    Add some personalization

--- app/src/main/java/com/csc/lpaina/lesson5/MainActivity.java
+++ app/src/main/java/com/csc/lpaina/lesson5/MainActivity.java
@@ -1,7 +1,7 @@
-package com.csc.lesson5;
+package com.csc.lpaina.lesson5;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
