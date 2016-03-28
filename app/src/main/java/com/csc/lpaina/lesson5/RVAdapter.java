3a85a77e5b7262de467e13d80247b03e26b62b70
lpaina <lubashka.994@mail.ru>
2016-03-28 03:50:13 +0300

    Add onClickListener

--- app/src/main/java/com/csc/lpaina/lesson5/RVAdapter.java
+++ app/src/main/java/com/csc/lpaina/lesson5/RVAdapter.java
@@ -1,11 +1,14 @@
 package com.csc.lpaina.lesson5;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -31,8 +34,17 @@ public class RVAdapter extends RecyclerView.Adapter<RVAdapter.FileViewHolder> {
     }
 
     @Override
-    public FileViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
-        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.recyclerview_item, viewGroup, false);
+    public FileViewHolder onCreateViewHolder(ViewGroup viewGroup, final int i) {
+        final Context context = viewGroup.getContext();
+        View view = LayoutInflater.from(context).inflate(R.layout.recyclerview_item, viewGroup, false);
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FrameLayout frameLayout = (FrameLayout) v;
+                TextView textView = (TextView) frameLayout.findViewById(R.id.file_name);
+                Toast.makeText(context, "Clicked on " + textView.getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
         return new FileViewHolder(view);
     }
 
@@ -47,4 +59,5 @@ public class RVAdapter extends RecyclerView.Adapter<RVAdapter.FileViewHolder> {
             fileIcon = (ImageView) itemView.findViewById(R.id.file_icon);
         }
     }
+
 }
