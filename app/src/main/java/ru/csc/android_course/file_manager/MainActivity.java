4398cf418dce83178a86489228e0893df13211f6
qurbonzoda <a.qurbonzoda@gmail.com>
2016-03-28 14:25:47 +0300

    done. basic features except last term

--- app/src/main/java/ru/csc/android_course/file_manager/MainActivity.java
+++ app/src/main/java/ru/csc/android_course/file_manager/MainActivity.java
@@ -1,11 +1,20 @@
 package ru.csc.android_course.file_manager;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.webkit.MimeTypeMap;
+import android.widget.Toast;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
@@ -33,8 +42,60 @@ public class MainActivity extends AppCompatActivity {
         }
 
         ArrayList<File> files = new ArrayList<>();
-        Collections.addAll(files, new File(directory).listFiles());
 
-        recyclerView.setAdapter(new FileAdapter(files));
+        File file = new File(directory);
+        if (file.listFiles() != null) {
+            Collections.addAll(files, file.listFiles());
+        }
+
+        recyclerView.setAdapter(new FileAdapter(files, this));
     }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString(DIRECTORY, directory);
+    }
+
+    public void onFolderClick(File file) {
+        Intent intent = new Intent(this, MainActivity.class);
+        try {
+            intent.putExtra(DIRECTORY, file.getCanonicalPath());
+            startActivity(intent);
+        } catch (IOException e) {
+            Toast.makeText(this, "Sorry, we couldn't open the folder.", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void onFileClick(File file) {
+        try {
+            MimeTypeMap myMime = MimeTypeMap.getSingleton();
+            Intent newIntent = new Intent(Intent.ACTION_VIEW);
+            String mimeType = myMime.getMimeTypeFromExtension(fileExt(file.getAbsolutePath()).substring(1));
+            newIntent.setDataAndType(Uri.fromFile(file), mimeType);
+            newIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(newIntent);
+        } catch (ActivityNotFoundException | NullPointerException e) {
+            Toast.makeText(this, "No handler for this type of file.", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private String fileExt(String url) {
+        if (url.contains("?")) {
+            url = url.substring(0, url.indexOf("?"));
+        }
+        if (url.lastIndexOf(".") == -1) {
+            return null;
+        } else {
+            String ext = url.substring(url.lastIndexOf("."));
+            if (ext.contains("%")) {
+                ext = ext.substring(0, ext.indexOf("%"));
+            }
+            if (ext.contains("/")) {
+                ext = ext.substring(0, ext.indexOf("/"));
+            }
+            return ext.toLowerCase();
+        }
+    }
+
 }
